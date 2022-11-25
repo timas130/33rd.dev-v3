62220047d39b439019a307bdec3034755b075cc3
sitting33 <me@sit.sh>
2022-11-25 18:21:42 +0300

    add email "protection"

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function setting(name) {
 
 setting("square-avatar");
 setting("light-theme");
+
+const el = document.getElementById("email");
+setTimeout(() => {
+  const part1 = "me";
+  const part3 = "sit.s";
+  const part2 = "@";
+  el.href = "mail" + "to:" + part1 + part2 + part3 + "h";
+  el.innerHTML = part1 + part2 + part3 + "h";
+}, 1200);
